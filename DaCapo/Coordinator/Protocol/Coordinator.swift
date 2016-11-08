52eb68e1ac4d90fa2e67fd0bd12809cb39eb8080
Thomas Segkoulis <segkoulis@gmail.com>
2016-11-08 07:54:48 +0100

    added pods, converted 'Reusable' to swift 3

---
+++ DaCapo/Coordinator/Protocol/Coordinator.swift
@@ -0,0 +1,14 @@
+//
+//  Coordinator.swift
+//  DaCapo
+//
+//  Created by Thomas Segkoulis on 08/11/16.
+//  Copyright © 2016 Thomas Segkoulis. All rights reserved.
+//
+
+import UIKit
+
+protocol Coordinator: class
+{
+    func start()
+}
